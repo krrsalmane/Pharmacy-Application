1caf6b2ad7a742f59fc22cfa59c766bc0d5c2930
krrsalmane <karrounsalmane@gmail.com>
2025-05-07 15:30:10 +0100

    delete product

--- pharmacy/src/main/java/com/Application/pharmacy/service/ProductService.java
+++ pharmacy/src/main/java/com/Application/pharmacy/service/ProductService.java
@@ -25,5 +25,11 @@ public class ProductService {
     public Product addProduct(Product product) {
         return productRepository.save(product);
     }
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+    public void deleteProduct(long id) {
+        productRepository.deleteById(id);
+    }
 
 }
