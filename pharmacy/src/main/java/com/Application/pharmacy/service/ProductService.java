59eea3ecf62638aaa85f89a8e68868cbb062299b
krrsalmane <karrounsalmane@gmail.com>
2025-05-07 15:29:33 +0100

    add a product

---
+++ pharmacy/src/main/java/com/Application/pharmacy/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.Application.pharmacy.service;
+
+import com.Application.pharmacy.model.Product;
+import com.Application.pharmacy.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    @Autowired
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    public Optional<Product> getProductById(long id) {
+        return productRepository.findById(id);
+    }
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+}
