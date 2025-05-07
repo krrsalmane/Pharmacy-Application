a89d2c49f8a4e44956dff84527753c907702eba5
krrsalmane <karrounsalmane@gmail.com>
2025-05-07 15:15:47 +0100

    create the model

--- pharmacy/src/main/java/com/Application/pharmacy/model/Product.java
+++ pharmacy/src/main/java/com/Application/pharmacy/model/Product.java
@@ -40,5 +40,27 @@ public class Product {
         this.name = name;
     }
 
+    public double getPrice() {
+        return price;
+    }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
