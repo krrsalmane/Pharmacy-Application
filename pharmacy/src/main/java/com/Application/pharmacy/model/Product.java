15e9e042673dada8030121ab994f337cd20bbd5d
krrsalmane <karrounsalmane@gmail.com>
2025-05-07 15:15:02 +0100

    first commit

---
+++ pharmacy/src/main/java/com/Application/pharmacy/model/Product.java
@@ -0,0 +1,44 @@
+package com.Application.pharmacy.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+
+    public Product(int id, String name, double price, int quantity, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
